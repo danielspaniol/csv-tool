e13964d763984ea17b7d7921c16a9d11e6b9c5d2
Daniel Spaniol <mail@daniel-spaniol.de>
2021-05-26 10:18:41 +0200

    Refactoring, sorting & UI upgrade

--- script.js
+++ script.js
@@ -1,54 +1,178 @@
+////////////////////////////////////////////////////////////////////////////////
+// INITIALIZATION
+////////////////////////////////////////////////////////////////////////////////
+
+window.onload = readCachedFiles
+
+////////////////////////////////////////////////////////////////////////////////
+// GLOBALS & CONSTANTS
+////////////////////////////////////////////////////////////////////////////////
+
 var OWNER_IDX = 11;
 var ID_IDX = 1;
 var T_IDX = 2;
 
-function handleFiles() {
-    var el = document.getElementById("upload");
-    var resultList = document.getElementById("results");
-    for (var f of el.files) {
+let contents = [];
 
-        var reader = new FileReader();
-        reader.fileName = f.name;
-        reader.onload = function (e) {
-            var num_ts = 0;
-            var num_ts_per_owner = {};
-            var owners = new Set();
-            var content = e.target.result;
+////////////////////////////////////////////////////////////////////////////////
+// GUI
+////////////////////////////////////////////////////////////////////////////////
 
-            var rows = content.split("\n");
-            for (var r of rows) {
-                var cols = r.split(";");
+let BUSY_INDICATOR_CLASS = "upload-indicator__background";
+let BUSY_INDICATOR_INACTIVE_CLASS = "upload-indicator__background--inactive";
 
-                if (cols.length > OWNER_IDX) {
-                    var owner = cols[OWNER_IDX];
-                    var id = cols[ID_IDX];
+let TOTAL_COUNT_CLASS = "upload-indicator__total-count";
+let DONE_COUNT_CLASS = "upload-indicator__done-count";
 
-                    owners.add(owner)
+function showBusyIndicator(totalFiles) {
+    updateTotalCount(totalFiles);
+    updateDoneCount(0);
 
-                    if (id && id.charAt(T_IDX) == 't') {
-                        num_ts_per_owner[owner] = (num_ts_per_owner[owner] || 0) + 1;
-                        ++num_ts;
-                    }
-                }
-            };
-
-            var docText = e.target.fileName + " (" + num_ts + " t)";
-            var nameDOM = document.createElement("li");
-            nameDOM.appendChild(document.createTextNode(docText));
-            resultList.appendChild(nameDOM);
-
-
-            var ownersDOM = document.createElement("ul");
-            for (var o of owners) {
-                var ownerText = o + " (" + (num_ts_per_owner[o] || 0) + " t)";
-
-                var ownerDOM = document.createElement("li");
-                ownerDOM.appendChild(document.createTextNode(ownerText));
-                ownersDOM.appendChild(ownerDOM);
-            }
-            nameDOM.appendChild(ownersDOM);
-        };
-
-        reader.readAsText(f);
+    let indEls = document.getElementsByClassName(BUSY_INDICATOR_CLASS);
+    for (let indEl of indEls) {
+        indEl.classList.remove(BUSY_INDICATOR_INACTIVE_CLASS);
     }
 }
+
+function hideBusyIndicator() {
+    let indEls = document.getElementsByClassName(BUSY_INDICATOR_CLASS);
+    for (let indEl of indEls) {
+        indEl.classList.add(BUSY_INDICATOR_INACTIVE_CLASS);
+    }
+}
+
+function updateTotalCount(totalFiles) {
+    let totalEls = document.getElementsByClassName(TOTAL_COUNT_CLASS);
+    for (let totalEl of totalEls) {
+        totalEl.innerHTML = "" + totalFiles;
+    }
+}
+
+function updateDoneCount(doneFiles) {
+    let doneEls = document.getElementsByClassName(DONE_COUNT_CLASS);
+    for (let doneEl of doneEls) {
+        doneEl.innerHTML = "" + doneFiles;
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// IO
+////////////////////////////////////////////////////////////////////////////////
+
+function readCachedFiles() {
+    let el = document.getElementById("upload");
+    if (el.files && el.files.length > 0) { readFiles(); }
+}
+
+function readFiles() {
+    let el = document.getElementById("upload");
+
+    let doneFiles = 0;
+    let totalFiles = el.files.length;
+
+    showBusyIndicator(totalFiles);
+
+    for (var f of el.files) {
+        readFile(f, function (name, content) {
+            contents.push({ name, content });
+            ++doneFiles;
+
+            updateDoneCount(doneFiles);
+
+            if (doneFiles == totalFiles) {
+                contents.sort(function (a, b) { return a.name.localeCompare(b.name) });
+                setTimeout(hideBusyIndicator, 500);
+            }
+        });
+    }
+}
+
+function readFile(file, callback) {
+    let reader = new FileReader();
+    reader.fileName = file.name;
+    reader.onload = function (e) {
+        let csv = e.target.result;
+        let content = deserialize(csv);
+        callback(e.target.fileName, content);
+    }
+    reader.readAsText(file);
+}
+
+function deserialize(csv) {
+    csv = csv || "";
+
+    let rows = csv.split("\n").filter(function (row) { return row && row.length > 0; });
+    let cols = rows.map(function (row) { return row.split(";") });
+
+    return cols;
+}
+
+function addOwnersResult(fileName, owners, numTs, numTsPerOwner) {
+    let resultEl = document.getElementById("results");
+    let [fileEl, fileListEl] = createFileEntry(fileName + " (" + numTs + " t)");
+
+    for (let o of owners) {
+        fileListEl.appendChild(createOwnerEntry(o, numTsPerOwner[o] || "0"));
+    }
+
+    resultEl.appendChild(fileEl);
+}
+
+function createFileEntry(fileName) {
+    let nameDOM = document.createElement("li");
+    nameDOM.appendChild(document.createTextNode(fileName));
+    let listDOM = document.createElement("ul");
+    nameDOM.appendChild(listDOM);
+
+    return [nameDOM, listDOM];
+}
+
+function createOwnerEntry(owner, numTs) {
+    var nameDOM = document.createElement("li");
+    nameDOM.appendChild(document.createTextNode(owner + " (" + numTs + " t)"));
+    return nameDOM;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// LOGIC
+////////////////////////////////////////////////////////////////////////////////
+
+function listOwners() {
+    let doneFiles = 0;
+    let totalFiles = contents.length;
+
+    showBusyIndicator(totalFiles);
+
+    for (let file of contents) {
+        let owners = new Set();
+
+        let numTs = 0;
+        let numTsPerOwner = {};
+
+        for (let row of file.content) {
+            let owner = row[OWNER_IDX];
+            let id = row[ID_IDX];
+
+            owners.add(owner);
+
+            if (id && id.charAt(T_IDX) == 't') {
+                numTsPerOwner[owner] = (numTsPerOwner[owner] || 0) + 1;
+                ++numTs;
+            }
+        }
+
+        addOwnersResult(file.name, owners, numTs, numTsPerOwner)
+
+        ++doneFiles;
+        updateDoneCount(doneFiles);
+        if (doneFiles == totalFiles) {
+            setTimeout(hideBusyIndicator, 500);
+        }
+    }
+
+}
+
+function listEstates() {
+
+}
+
